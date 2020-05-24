913398897fc2346438a5572cf9a3c33a49316f4b
Sathya LK <rajavslk@gmail.com>
2020-05-24 14:45:02 +0530

    added dummy data

---
+++ server/dev-data/dummy-data.js
@@ -0,0 +1,14 @@
+let books = [
+  { name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
+  { name: 'The Final Empire', genre: 'Fantasy', id: '2' },
+  { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
+];
+
+let authors = [
+  { name: 'Patrick Rothfuss', age: 44, id: '1' },
+  { name: 'Grant Sanderson', age: 42, id: '2' },
+  { name: 'Michael Stevens', age: 69, id: '3' },
+];
+
+module.exports = books;
+module.exports = authors;
